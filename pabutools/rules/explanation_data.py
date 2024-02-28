253fe7c2983dd06b5658b1af3df7808c8f328fc1
Leon Chipchase <leonchipchase2@gmail.com>
2024-02-28 16:33:33 +0000

    Merging James's Changes

--- pabutools/rules/explanation_data.py
+++ pabutools/rules/explanation_data.py
@@ -6,6 +6,7 @@ class MESData(ExplanationData):
         self.rounds = []
 
     def record_round_start(self, projects):
+        projects = sorted(projects, key=lambda p: 1/p.affordability, reverse=False)
         round = {
             "effective_vote_count": {
                 p.name: float(1/p.affordability) for p in projects
@@ -14,6 +15,7 @@ class MESData(ExplanationData):
         self.rounds.append(round)
 
     def record_round_end(self, projects, selected_project):
+        projects = sorted(projects, key=lambda p: 1/p.affordability, reverse=False)
         self.rounds[-1]["effective_vote_count_reduction"] = {
             p.name: float(self.rounds[-1]["effective_vote_count"][p]-1/p.affordability) for p in projects
         }
